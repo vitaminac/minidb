9f0e992d272b8b264578b2d1b717f2fd817b96b3
daolao <dalao1002@gmail.com>
2018-08-03 21:44:52 +0200

    global test, run at final

--- src/test/java/scheduler/SchedulerTest.java
+++ src/test/java/scheduler/SchedulerTest.java
@@ -1,111 +1,135 @@
 package scheduler;
 
 import nio.NIOServerSocket;
-import nio.ServerSocketHandler;
-import nio.SocketHandler;
-import org.junit.Before;
+import org.junit.AfterClass;
 import org.junit.Test;
 import promise.Promise;
 
-import java.nio.ByteBuffer;
-import java.nio.CharBuffer;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static scheduler.SchedulerHelper.run;
+import static scheduler.SchedulerHelper.setImmediate;
 import static scheduler.SchedulerHelper.setTimeout;
 
 public class SchedulerTest {
     private static final int PORT = 1338;
-    private StringBuilder sb = new StringBuilder();
+    private static List<Object> results = new ArrayList<>();
+    private NIOServerSocket server;
 
-    @Before
-    public void setUp() throws Exception {
-        NIOServerSocket.listen(PORT, new ServerSocketHandler() {
-                    @Override
-                    public void onClose() {
-                        System.out.println("Closing server");
-                    }
+//    @Before
+//    public void setUp() throws Exception {
+//        this.server = NIOServerSocket.listen(PORT, new ServerSocketHandler() {
+//                    @Override
+//                    public void onError(Exception e) {
+//                        SchedulerTest.this.results.add("This should not be called\n");
+//                    }
+//
+//                    @Override
+//                    public void onClose() {
+//                        SchedulerTest.this.results.add("Closing server\n");
+//                    }
+//
+//                    @Override
+//                    public SocketHandler buildSocketHandler() {
+//                        return new SocketHandler() {
+//                            @Override
+//                            public void onConnect() {
+//                                SchedulerTest.this.sb.append("Connected to the server\n");
+//                            }
+//
+//
+//                            @Override
+//                            public void onData(ByteBuffer data) {
+//                                CharBuffer charBuffer = StandardCharsets.UTF_8.decode(data);
+//                                String text = charBuffer.toString();
+//                                SchedulerTest.this.sb.append(text);
+//                            }
+//
+//                            @Override
+//                            public void onError(Exception e) {
+//                                SchedulerTest.this.sb.append("This should not be called\n");
+//                            }
+//
+//                            @Override
+//                            public void onClose() {
+//                                SchedulerTest.this.sb.append("Closing the client\n");
+//                            }
+//                        };
+//                    }
+//                }
+//        );
+//    }
 
-                    @Override
-                    public SocketHandler buildSocketHandler() {
-                        return new SocketHandler() {
-                            private final StringBuilder sb = SchedulerTest.this.sb;
-
-                            @Override
-                            public void onData(ByteBuffer data) {
-                                CharBuffer charBuffer = StandardCharsets.UTF_8.decode(data);
-                                String text = charBuffer.toString();
-                                sb.append(text);
-                            }
-
-                            @Override
-                            public void onClose() {
-                                System.out.println(sb.toString());
-
-                            }
-                        };
-                    }
-                }
-        );
-    }
-
-    @Test
-    public void testEventLoop() throws Exception {
+    @AfterClass
+    public static void tearDown() {
         run();
+        System.out.println(results);
+        assertArrayEquals(new Object[]{1, 3, 5, 6, 7, 8, 9, 11, 12, 14}, results.toArray());
     }
 
     @Test
-    public void test() {
-        List<Integer> order = new ArrayList<>();
+    public void setTimeoutTest() {
         Promise.from((DeferredTask<Integer>) promise -> setTimeout(() -> {
-            order.add(6);
-            promise.resolve(6);
-        }, 2000)).onFulfilled(value -> {
-            order.add(7);
-            return 7;
-        }).onFulfilled((result -> {
-            order.add(8);
-            throw new Exception("8");
-        })).onRejected(e -> {
-            order.add(9);
-            return 9;
+            int retVal = 12;
+            results.add(retVal);
+            promise.reject(new TestException(retVal));
+        }, 501)).onFulfilled(result -> {
+            int retVal = 13;
+            results.add(retVal);
+            return retVal;
         }).onRejected(e -> {
-            order.add(10);
-            return 10;
-        }).onFinally(() -> order.add(11));
-
-        Promise.from((DeferredTask<Integer>) promise -> setTimeout(() -> {
-            order.add(1);
-            promise.reject(new Exception("1"));
-        }, 1500)).then(result -> {
-            order.add(2);
-            return 2;
-        }, e -> {
-            order.add(3);
-            return 3;
-        }).onRejected(e -> {
-            order.add(4);
-            return 4;
-        }).onFinally(() -> order.add(5));
-
-        Promise.from((DeferredTask<Integer>) promise -> setTimeout(() -> {
-            order.add(12);
-            promise.reject(new Exception("from here"));
-        }, 3000)).onFulfilled(result -> {
-            order.add(13);
-            return 13;
-        }).onRejected(e -> {
-            order.add(14);
-            return 14;
+            assertEquals(12, ((TestException) e).getValue());
+            int retVal = 14;
+            results.add(retVal);
+            return retVal;
         });
 
-        run();
-        int[] result = order.stream().mapToInt(i -> i).toArray();
-        System.out.println(Arrays.toString(result));
-        assertArrayEquals(new int[]{1, 3, 5, 6, 7, 8, 9, 11, 12, 14}, result);
+        Promise.from((DeferredTask<Integer>) promise -> setTimeout(() -> {
+            int retVal = 6;
+            results.add(retVal);
+            promise.resolve(retVal);
+        }, 500)).onFulfilled(result -> {
+            assertEquals(6, result.intValue());
+            int retVal = 7;
+            results.add(retVal);
+            return retVal;
+        }).onFulfilled((result -> {
+            assertEquals(7, result.intValue());
+            int retVal = 8;
+            results.add(retVal);
+            throw new TestException(retVal);
+        })).onRejected(e -> {
+            assertEquals(8, ((TestException) e).getValue());
+            int retVal = 9;
+            results.add(retVal);
+            return retVal;
+        }).onRejected(e -> {
+            results.add(10);
+            return 10;
+        }).onFinally(() -> results.add(11));
+    }
+
+    @Test
+    public void setImmediateTest() {
+        Promise.from((DeferredTask<Integer>) promise -> setImmediate(() -> {
+            int retVal = 1;
+            results.add(retVal);
+            promise.reject(new TestException(retVal));
+        })).then(result -> {
+            results.add(2);
+            return 2;
+        }, e -> {
+            assertEquals(1, ((TestException) e).getValue());
+            int retVal = 3;
+            results.add(retVal);
+            return retVal;
+        }).onRejected(e -> {
+            int retVal = 4;
+            results.add(retVal);
+            return retVal;
+        }).onFinally(() -> results.add(5));
     }
 }
