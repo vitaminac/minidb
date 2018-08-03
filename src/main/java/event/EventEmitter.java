1eb040d05509399c9cd7d747c31e9aaa7cc05f44
daolao <dalao1002@gmail.com>
2018-08-03 21:43:17 +0200

    fix use the global default loop

--- src/main/java/event/EventEmitter.java
+++ src/main/java/event/EventEmitter.java
@@ -6,15 +6,13 @@ import observer.Subscriber;
 import java.util.HashSet;
 import java.util.Set;
 
-import static scheduler.Scheduler.setImmediate;
-
 public class EventEmitter<E> implements Observable<E> {
     private final Set<Subscriber<? super E>> subscribers = new HashSet<>();
 
     @Override
     public synchronized void publish(E event) {
         for (Subscriber<? super E> subscriber : this.subscribers) {
-            setImmediate(() -> subscriber.notify(event));
+            subscriber.notify(event);
         }
     }
 
