797184d7b350dd658a6a2a3fdadcf434f3dd269f
dalao <dalao1002@gmail.com>
2019-12-07 11:27:43 +0100

    PING PONG client server

--- src/main/java/MiniDBClient.java
+++ src/main/java/MiniDBClient.java
@@ -1,5 +1,25 @@
-public class MiniDBClient {
-    public static void main(String... args) {
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class MiniDBClient {
+    public static void main(String... args) throws IOException {
+        // TODO: get host and port from configuration file
+        try (Socket socket = new Socket("localhost", 9000)) {
+            try (
+                    var scanner = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
+                    var writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())));
+                    var reader = new Scanner(new BufferedReader(new InputStreamReader(socket.getInputStream())));
+            ) {
+                while (!socket.isClosed()) {
+                    if (scanner.hasNext()) {
+                        var line = scanner.nextLine();
+                        writer.println(line);
+                        writer.flush();
+                        System.out.println(reader.nextLine());
+                    }
+                }
+            }
+        }
     }
 }
--- src/main/java/MiniDBServer.java
+++ src/main/java/MiniDBServer.java
@@ -1,29 +1,67 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class MiniDBServer {
 
-    public static void main(String... args) {
-        try (var sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)))) {
-            var ended = false;
-            while (!ended) {
-                if (sc.hasNext()) {
-                    var line = sc.nextLine();
-                    var tokenizer = new StringTokenizer(line);
-                    if (tokenizer.countTokens() > 0) {
-                        var command = tokenizer.nextToken();
-                        if (command.equals("PING")) {
-                            System.out.println("PONG");
-                        } else {
-                            System.out.println("ERROR: unknown command " + command);
+    private static class ClientHandler implements Runnable {
+        private final Socket socket;
+
+        private ClientHandler(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            // TODO: NON-BLOCKING
+            try (var reader = new Scanner(this.socket.getInputStream());
+                 var writer = new PrintWriter(this.socket.getOutputStream())
+            ) {
+                while (!this.socket.isClosed()) {
+                    if (reader.hasNext()) {
+                        var line = reader.nextLine();
+                        System.out.println("Received: " + line);
+                        var tokenizer = new StringTokenizer(line);
+                        if (tokenizer.countTokens() > 0) {
+                            var command = tokenizer.nextToken();
+                            if (command.equals("PING")) {
+                                writer.println("PONG");
+                                writer.flush();
+                            } else if (command.equals("QUIT")) {
+                                writer.write("QUITING");
+                                writer.flush();
+                                this.socket.close();
+                            } else {
+                                writer.println("ERROR: unknown command " + command);
+                                writer.flush();
+                            }
                         }
                     }
-                } else {
-                    Thread.yield();
+                }
+            } catch (IOException e) {
+                // TODO: logging
+                e.printStackTrace();
+            } finally {
+                try {
+                    this.socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
         }
+    }
+
+    // TODO: java.nio
+    public static void main(String... args) throws IOException {
+        // TODO get port from args
+        var serverSocket = new ServerSocket(9000);
+        while (!serverSocket.isClosed()) {
+            var client = serverSocket.accept();
+            System.out.println("Received new connection from " + client.getRemoteSocketAddress().toString());
+            new Thread(new ClientHandler(client)).start();
+        }
+        // TODO: use executor service
     }
 }
